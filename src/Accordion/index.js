b4562b4b438e7b156c0a0e21c63c691fadc6215f
Melany <hjose0650@gmail.com>
2022-11-11 11:30:22 -0500

    add navbar

---
+++ src/Accordion/index.js
@@ -0,0 +1,14 @@
+// import react from "react";
+import './style.scss'
+
+const Accordion = ({
+    title,
+    parraph,
+    img,    
+}) =>{
+    return(
+            <div className='accordion'>
+                <h1 className='title'>{title}</h1>
+            </div>
+    );
+}
