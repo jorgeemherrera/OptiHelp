4fdd053b15be13f738b9f70c2abb0bdfebb128a0
Melany <hjose0650@gmail.com>
2022-11-11 17:24:48 -0500

    add accordion style

--- src/App.js
+++ src/App.js
@@ -44,9 +44,62 @@ function App() {
           "
         >
           <div className='poitns-accordion'>
-            <section className='section-first-point'>
+            <section className='section first-point'>
               <p>1. Usar prescripción óptica: No olvides asistir al optómetra una vez al año para actualizar tu fórmula de gafas.</p>
-              <img src="https://static.toiimg.com/thumb/75181065.cms?width=680&height=512&imgsize=200465" />
+              <img src="https://static.toiimg.com/thumb/75181065.cms?width=680&height=512&imgsize=200465" alt='girl-with-glasses' />
+              <p>Para mayor información…(aparecerá link para ingresar a la presentación en power point)</p>
+            </section>
+            <section className='section secopnd-point'>
+              <p>2. Mantener buena iluminación: Evita trabajar en lugares con baja iluminación para evitar fatiga visual.</p>
+              <img src="" alt='' />
+              <h3>Trabaja en ambientes con iluminación artificial y natural, evitando el cansancio ocular</h3>
+              <p>Para mayor información…(aparecerá link para ingresar a la presentación en power point)</p>
+            </section>
+            <section className='section third-point'>
+              <p>3. Distancia de trabajo: No acercar el celular a los ojos ya que ocasiona mayor cansancio y estrés visual.</p>
+              <img src="" alt='' />
+              <h3>Usa como mínimo la distancia entre el codo y tú mano cuando uses el celular</h3>
+              <p>Para mayor información…(aparecerá link para ingresar a la presentación en power point)</p>
+            </section>
+            <section className='section fourth-point'>
+              <p>4. Postura: La distancia entre el computador y los ojos debe estar entre 45cm y 75cms..</p>
+              <img src="" alt='' />
+              <p>Para mayor información…(aparecerá link para ingresar a la presentación en power point)</p>
+            </section>
+            <section className='section five-point'>
+              <p>5. Ambiente: Evita exponerte directamente al aire acondicionado ya que puede causar ojo seco y cansancio ocular</p>
+              <img src="" alt='' />
+              <h3>No te expongas a temperaturas muy bajas, ni muy altas para no generar cansancio ocular e inestabilidad lagrimal</h3>
+              <p>Para mayor información…(aparecerá link para ingresar a la presentación en power point)</p>
+            </section>
+            <section className='section six-point'>
+              <p>
+                6. Alimentación: Consume alimentos ricos en vitamina A (espinacas, papaya, tomate,
+                huevos y mantequilla), C (naranja, limón, piña, fresas, repollo), y E (nueces,
+                almendras, maní y avellanas). Nos ayuda a prevenir inestabilidad ocular.
+              </p>
+              <img src="" alt='' />
+              <h3>Balancea tú dieta con alimentos ricos en Omega3</h3>
+              <p>Para mayor información…(aparecerá link para ingresar a la presentación en power point)</p>
+            </section>
+            <section className='section seven-point'>
+              <p>
+                7. Pausas activas: Es necesario hacer pausas activas para disminuir el uso excesivo de
+                la visión próxima y así se permite relajar la acomodación y disminuir el estrés
+                visual.
+              </p>
+              <img src="" alt='' />
+              <h3>Balancea tú dieta con alimentos ricos en Omega3</h3>
+              <p>Para mayor información…(aparecerá link para ingresar a la presentación en power point)</p>
+            </section>
+            <section className='section heigth-point'>
+              <p>
+                8. No olvides parpadear. Cuando nos concentramos, disminuimos el parpadeo hasta
+                5 veces por minuto, lo ideal es que parpadeemos entre 15 y 20 veces por minuto,
+                con el fin de proveer lubricación al ojo.
+              </p>
+              <img src="" alt='' />
+              <p>Para mayor información…(aparecerá link para ingresar a la presentación en power point)</p>
             </section>
           </div>
         </Accordion>
